ffc8448a1da0002cbf0ff8a6ff99dd504e49cf44
desire <kenmaks73@mail.ru>
2022-10-23 17:43:08 +0300

    начальная реализация класса Ship

--- src/interface/SpaceWar/ship.cpp
+++ src/interface/SpaceWar/ship.cpp
@@ -1,1 +1,16 @@
 #include "ship.h"
+
+Ship::Ship() {
+    sf::Texture tmpTexture;
+    tmpTexture.loadFromFile("../Textures/SpaceWar/Ship/ShipLeft.png");
+    sprites[0] = sf::Sprite(tmpTexture);
+    tmpTexture.loadFromFile("../Textures/SpaceWar/Ship/ShipRight.png");
+    sprites[1] = sf::Sprite(tmpTexture);
+    tmpTexture.loadFromFile("../Textures/SpaceWar/Ship/Ship.png");
+    sprites[2] = sprites[3] = sf::Sprite(tmpTexture);
+    width = 30;
+    height = 42;
+    x = 585;
+    y = 600;
+}
+
--- src/interface/SpaceWar/ship.h
+++ src/interface/SpaceWar/ship.h
@@ -1,2 +1,23 @@
 #include "SFML/Graphics.hpp"
+#include "array"
 
+// на самом деле универсальнее было бы сделать класс, содержащий текстурки координаты и тд любого объекта, но так как это мини игра - мне впадлу
+
+class Ship{
+public:
+    Ship();
+
+    enum Directs{
+        Left = 0,
+        Right = 1,
+        Forward= 2,
+        Back = 3
+    };
+private:
+    Directs direct = Directs::Forward;
+    std::array<sf::Sprite, 4> sprites;
+    float x;
+    float y;
+    uint16_t width;
+    uint16_t height;
+};
